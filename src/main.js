e38492255569a804610c0af0273e5e487fc898c3
sumitra tamang <sumi8tamang@gmail.com>
2024-09-08 18:09:16 +0545

    feat:created pinia from pinia and used it

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import './assets/main.css'
-
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './Routes/routes'
-createApp(App).use(router).mount('#app')
+const pinia = createPinia()
+createApp(App)
+.use(pinia)
+.use(router)
+.mount('#app')
