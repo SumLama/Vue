7caccef3fceca8a4e249c77e8f42a4078a7cfca0
sumitra tamang <sumi8tamang@gmail.com>
2024-09-09 18:03:49 +0545

    feat: remove unnecessary importing of components from pages

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <script setup>
-import Products from './Pages/Products.vue';
-import ProductDetails from './Pages/ProductDetails.vue';
-import CartLisiting from './Pages/CartLisiting.vue';
 import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
   <Navbar/>
   <RouterView/>
-
- <!-- <Products/>  -->
- <!-- <ProductDetails/> -->
-  <!-- <CartLisiting/> -->
 </template>
 
 <style scoped>
