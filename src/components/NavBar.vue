042bda6840b0414c3133f635b1bc021ba56f28f4
sumitra tamang <sumi8tamang@gmail.com>
2024-09-09 18:10:07 +0545

    feat: convert the name of component to camelCase

---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { cartStore } from '@/stores/cartStores';
+const cart = cartStore()
+</script>
+<template>   
+    <div>
+        <ul class=" grid grid-cols-2 justify-items-end gap-10 text-2xl my-5 mx-2 ">
+            <li><router-link to="/" class="hover:text-orange-700">Home</router-link></li>
+            <li><router-link to="/cartlist" class="hover:text-orange-700 pr-12"><i class="fa fa-cart-shopping"></i>
+                <span class="text-base align-text-top mx-1">{{ cart.itemCount }}</span>
+            </router-link></li>    
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
