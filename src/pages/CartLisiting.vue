079c2608533024cc0705e87f3cd1394f3179c3aa
sumitra tamang <sumi8tamang@gmail.com>
2024-09-09 12:36:40 +0545

    feat: changes the case of folder name

---
+++ src/pages/CartLisiting.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+import { cartStore } from '@/stores/cartStores';
+const cart= cartStore()
+const cartItem = cart.items;
+const totalprice = cart.totalPrice;
+console.log(totalprice)       
+</script>
+<template>
+    <div v-if="cartItem.length > 0" class="w-full flex justify-center my-8">
+      <table class="w-4/5  bg-white border border-gray-200">
+        <thead>
+          <tr class="bg-gray-200">
+            <th class="py-2 px-4 border-b">Index</th>
+            <th class="py-2 px-4 border-b">Products</th>
+            <th class="py-2 px-4 border-b">Quantity</th>
+            <th class="py-2 px-4 border-b">Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in cartItem" :key="item.id" class="hover:bg-gray-100">
+            <td class="py-2 px-4 border-b text-center">{{ index + 1 }}</td>
+            <td class="py-2 px-4 border-b flex items-center justify-center">
+            <img :src="item.image" :alt="item.title" class="w-24 h-24 object-contain" />
+          </td>
+            <td class="py-2 px-4 border-b text-center"><input  type="text" v-model="item.quantity" height=""></td>
+            <td class="py-2 px-4 border-b text-center">$ {{ item.price * item.quantity }}</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td colspan="4" class="py-2 px-4 pr-6 border-t text-center font-bold">Total: ${{ totalprice }}</td>         
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Products.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+
+const products = ref([]);
+const loading = ref(false);
+
+const fetchProducts = async () => {
+    loading.value = true;
+    try {
+        const response = await axios.get('https://fakestoreapi.com/products?limit=20');
+        console.log(response.data);
+        products.value = response.data;
+    } catch (error) {
+        console.log("error occurs:", error);
+    } finally {
+        loading.value = false;
+    }
+};
+
+onMounted(() => {
+    fetchProducts();
+});
+</script>
+
+<template>
+    <div v-if="loading" class="text-center text-2xl">
+        <h1>Loading....</h1>
+    </div>
+    <div v-else class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 justify-items-center gap-5 m-8 font-serif text-lg cursor-pointer">
+        <div v-for="product in products" :key="product.id" >
+            <ul>
+                <router-link :to="{ name: 'ProductDetails', params: { id: product.id } }">
+                    <li>
+                        <img :src="product.image" :alt="product.title" class="h-[220px] w-[220px] p-3 border-2 rounded-md hover:shadow hover:border-4 object-contain">
+                    </li>
+                </router-link>
+                <li class="my-3">
+                    <p>{{ product.title }}</p>
+                </li>
+                <li class="my-3 text-orange-700">
+                    <p>$ {{ product.price }}</p>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
