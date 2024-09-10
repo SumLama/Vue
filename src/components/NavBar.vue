3561fc69e6206f32869ba6de9eeda3a603d8e6e7
sumitra tamang <sumi8tamang@gmail.com>
2024-09-10 16:19:27 +0545

    style :justify navbar items center, removal of padding right from cart icon

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,9 +4,9 @@ const cart = cartStore()
 </script>
 <template>   
     <div>
-        <ul class=" grid grid-cols-2 justify-items-end gap-10 text-2xl my-5 mx-2 ">
+        <ul class="flex justify-center gap-5 text-2xl my-5 mx-2 ">
             <li><router-link to="/" class="hover:text-orange-700">Home</router-link></li>
-            <li><router-link to="/cartlist" class="hover:text-orange-700 pr-12"><i class="fa fa-cart-shopping"></i>
+            <li><router-link to="/cartlist" class="hover:text-orange-700 pr-6"><i class="fa fa-cart-shopping"></i>
                 <span class="text-base align-text-top mx-1">{{ cart.itemCount }}</span>
             </router-link></li>    
         </ul>
