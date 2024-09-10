50973bf991264147f9182358dd8c357e4c891bf8
sumitra tamang <sumi8tamang@gmail.com>
2024-09-10 16:01:59 +0545

    feat: define stored cartitems as object, perform operations using Composition api

--- src/stores/cartStores.js
+++ src/stores/cartStores.js
@@ -1,20 +1,26 @@
 import { defineStore } from "pinia";
- const cartStore = defineStore('cart',{
-    state:()=>({
-        items:[],
-    }),
-    getters: {
-        itemCount :(state)=> state.items.length,
-        totalPrice : (state)=>state.items.reduce((total,item)=>total+item.price*item.quantity,0)
-    
-    },
-    actions:{
-        addItem(item){
-            this.items.push(item)
+import { computed, ref } from "vue";
+ export const cartStore = defineStore('cart',()=>{
+    const items= ref({})
+    const itemCount = computed(()=>{ return Object.keys(items.value).length})
+    const totalPrice = computed(()=> {return Object.values(items.value).reduce((total,item)=>total+item.price*item.quantity,0)})
+    const addItem = (item)=>{
+        if(items.value[item.id]){
+            items.value[item.id].quantity += item.quantity
         }
-
+        else{
+            items.value[item.id] = item
+        }
     }
+    
+    const removeItem =(itemId)=>{
+        if(items.value[itemId]){
+            delete items.value[itemId]
+        }
+    }
+
+    return {items,itemCount,totalPrice,addItem,removeItem}
  })
-export {cartStore};
+
 
 
