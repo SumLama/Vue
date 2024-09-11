86da4c643fe245d9bf60e2fc129968e039880be1
sumitra tamang <sumi8tamang@gmail.com>
2024-09-11 10:11:30 +0545

    feat: add path for checkout

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,11 +3,13 @@ import Home from "@/pages/Home.vue";
 import ProductDetails from "@/pages/ProductDetails.vue";
 import Products from "@/pages/Products.vue";
 import CartLisiting from "@/pages/CartLisiting.vue";
+import CheckOut from "@/pages/CheckOut.vue";
 const routes =[
   
     { path:'/',component:Products},
     { path: '/products/:id', name: 'ProductDetails', component: ProductDetails, props: true },
     { path:'/cartlist',component:CartLisiting},
+    {path:'/checkout',component:CheckOut},
    
   
 ];
